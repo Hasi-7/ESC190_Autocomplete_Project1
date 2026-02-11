85f8b9d6d87ccede33523c6afd1832c861bea7d4
Hasnain Heryani <hasnainheryani@gmail.com>
2026-02-11 00:12:29 -0500

    Setup Launch and Tasks Json files

---
+++ autocomplete.c
@@ -0,0 +1,24 @@
+#include "autocomplete.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void read_in_terms(struct term **terms, int *pnterms, char *filename)
+{
+    
+}
+
+int lowest_match(struct term *terms, int nterms, char *substr)
+{
+    return 0;
+}
+
+int highest_match(struct term *terms, int nterms, char *substr)
+{
+    return 0;
+}
+
+void autocomplete(struct term **answer, int *n_answer, struct term *terms, int nterms, char *substr)
+{
+
+}
