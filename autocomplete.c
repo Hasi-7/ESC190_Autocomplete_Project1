abb074c8a661fd54e992a762e32037e4b43405c1
Hasnain Heryani <hasnainheryani@gmail.com>
2026-02-11 13:07:53 -0500

    Started project with read_in_terms

--- autocomplete.c
+++ autocomplete.c
@@ -3,9 +3,31 @@
 #include <stdlib.h>
 #include <string.h>
 
+int compare(struct term *term1, struct term *term2)
+{
+    return strcmp(term1->term, term2->term);
+}
+
 void read_in_terms(struct term **terms, int *pnterms, char *filename)
 {
-    
+    *terms = (term *)malloc(sizeof(term)*(*pnterms));
+    char line[200];
+    FILE *fp = fopen(filename, "r"); 
+    for(int i = 0; i < *pnterms; i++)
+    { 
+        fgets(line, sizeof(line), fp);
+        (*terms + i)->weight = atoi(line);
+        
+        const char delimiter[2] = "\t";
+        char *token;
+        token = strtok(line, delimiter);
+        printf("Line: %s\n", token);
+        while (token != NULL) {
+            token = strtok(NULL, delimiter);
+        }
+    } 
+    fclose(fp);
+    qsort(*terms, *pnterms, sizeof(terms), compare);
 }
 
 int lowest_match(struct term *terms, int nterms, char *substr)
--- main.c
+++ main.c
@@ -1,16 +1,28 @@
 #include "autocomplete.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void print_terms(struct term **terms, int *pnterms)
+{
+    for (int i = 0; i < *pnterms; i++)
+    {
+        printf("Term: %s, %d", (*terms + i)->term, (*terms + i)->weight);
+    }
+}
 
 int main(void)
 {
     struct term *terms;
     int nterms;
     read_in_terms(&terms, &nterms, "cities.txt");
-    lowest_match(terms, nterms, "Tor");
-    highest_match(terms, nterms, "Tor");
+    print_terms(&terms, &nterms);
+    //lowest_match(terms, nterms, "Tor");
+    //highest_match(terms, nterms, "Tor");
     
-    struct term *answer;
-    int n_answer;
-    autocomplete(&answer, &n_answer, terms, nterms, "Tor");
+    // struct term *answer;
+    // int n_answer;
+    // autocomplete(&answer, &n_answer, terms, nterms, "Tor");
     //free allocated blocks here -- not required for the project, but good practice
     return 0;
 }
